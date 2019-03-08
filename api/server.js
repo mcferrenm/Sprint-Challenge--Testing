a586508d0dd157528fbebfada77d04966c44ab2a
mcferrenm <mcferrenmax@gmail.com>
2019-03-08 11:17:58 -0500

    GET /games should return status code 200

--- api/server.js
+++ api/server.js
@@ -1,3 +1,7 @@
-const server = require('express')();
+const server = require("express")();
+
+server.get("/games", (req, res) => {
+  res.status(200).send("Hi");
+});
 
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,5 +1,12 @@
 const request = require('supertest');
+const server = require('./server.js');
 
 describe('/games', () => {
-  
+  describe('GET /', () => {
+    it('should return status code 200', async () => {
+      const res = await request(server).get('/games');
+
+      expect(res.status).toBe(200);
+    });
+  });
 });
