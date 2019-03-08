cdd67f685e326a01b834af707d3869322893ce6e
mcferrenm <mcferrenmax@gmail.com>
2019-03-08 12:04:44 -0500

    /games PUT should validate req, return 422 if incomplete

--- api/server.js
+++ api/server.js
@@ -1,9 +1,22 @@
-const server = require("express")();
+const express = require("express");
+
+const server = express();
 
 const games = [];
 
+server.use(express.json());
+
 server.get("/games", (req, res) => {
   res.status(200).json(games);
+});
+
+server.post("/games", (req, res) => {
+  if (!req.body.title || !req.body.genre) {
+    res.status(422).json({ error: "Must contain title and genre" });
+  } else {
+    games.push(req.body);
+    res.status(201).json(games);
+  }
 });
 
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,18 +1,35 @@
-const request = require('supertest');
-const server = require('./server.js');
+const request = require("supertest");
+const server = require("./server.js");
 
-describe('/games', () => {
-  describe('GET /', () => {
-    it('should return status code 200', async () => {
-      const res = await request(server).get('/games');
+describe("/games", () => {
+  describe("GET /", () => {
+    it("should return status code 200", async () => {
+      const res = await request(server).get("/games");
 
       expect(res.status).toBe(200);
     });
 
-    it('should always return an array', async () => {
-      const res = await request(server).get('/games');
+    it("should always return an array", async () => {
+      const res = await request(server).get("/games");
 
-      expect(res.body).toBeInstanceOf(Array)
+      expect(res.body).toBeInstanceOf(Array);
+    });
+  });
+
+  describe("POST /", () => {
+    it("should validate required fields inside the req body, and return 422 for incomplete information", async () => {
+      const valid = {
+        title: "Pacman", // required
+        genre: "Arcade" // required
+      };
+
+      const invalid = {};
+
+      const res = await request(server).post("/games").send(invalid);
+      expect(res.status).toBe(422);
+
+      const validRes = await request(server).post("/games").send(valid);
+      expect(validRes.status).toBe(201);
     });
   });
 });
